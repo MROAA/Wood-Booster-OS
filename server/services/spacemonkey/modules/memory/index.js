4f592e312ea8ddd98ed79d7386f9abac282ea10e
Marc Järvinen <mroaac@gmail.com>
2026-08-08 19:39:37 +0300

    Repair Spacemonkey architecture module entry points

---
+++ server/services/spacemonkey/modules/memory/index.js
@@ -0,0 +1,30 @@
+/*
+WOOD-BOOSTER HQ
+
+SPACEMONKEY
+
+MEMORY MODULE ENTRY POINT
+
+Vastuut:
+
+- tarjoaa Memory Engine moduulin
+- toimii moduulin julkisena rajapintana
+
+Ei:
+
+- muuta Memory Engine logiikkaa
+- käynnistä moottoria
+*/
+
+
+import MemoryEngine from "./MemoryEngine.js"
+
+
+export {
+
+MemoryEngine
+
+}
+
+
+export default MemoryEngine
