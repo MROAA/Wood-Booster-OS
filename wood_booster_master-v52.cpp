90bedbd6d1402b77c8ef0198e70e66363f08fc2e
Marc Järvinen <mroaac@gmail.com>
2026-08-13 11:15:44 +0300

    Wood-Booster OS v52: Windows96-työpöytätoiminnallisuus lisätty

---
+++ wood_booster_master-v52.cpp
@@ -0,0 +1,9 @@
+// Wood-Booster OS v52 - Win96 Integration
+#include <iostream>
+
+int main() {
+    std::cout << "Käynnistetään Wood-Booster OS v52 (Win96 Edition)...\n";
+    std::cout << "Moduulit ladattu: RANNIKKO, WIN96, KIVISTÖ, OPPI\n";
+    std::cout << "Win96 Työpöytä valmiina. Tervetuloa takaisin!\n";
+    return 0;
+}
